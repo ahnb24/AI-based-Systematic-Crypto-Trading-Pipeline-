9af4029d1627fadc90dbc1f23a3e393d19b2fb31
Amirhessam Nakhaei <ahnb24@gmail.com>
2025-10-21 16:35:41 +0100

    add train for realtime section

---
+++ train for realtime/configs/symbols_info.py
@@ -0,0 +1,120 @@
+
+
+
+symbols_dict = {
+    "BTCUSDT" :{
+        "decimal_divide": 1e5,
+        "pip_size": 1,
+        "binance_id": "BTCUSDT",
+        "swap_rate":{"long": 0, "short": 0}
+    },
+    "ADAUSDT" :{
+        "decimal_divide": 1e5,
+        "pip_size": 1,
+        "binance_id": "ADAUSDT",
+        "swap_rate":{"long": 0, "short": 0}
+    },
+    "BCHUSDT" :{
+        "decimal_divide": 1e5,
+        "pip_size": 1,
+        "binance_id": "BCHUSDT",
+        "swap_rate":{"long": 0, "short": 0}
+    },
+    "BNBUSDT" :{
+        "decimal_divide": 1e5,
+        "pip_size": 1,
+        "binance_id": "BNBUSDT",
+        "swap_rate":{"long": 0, "short": 0}
+    },
+    "DOGEUSDT" :{
+        "decimal_divide": 1e5,
+        "pip_size": 1,
+        "binance_id": "DOGEUSDT",
+        "swap_rate":{"long": 0, "short": 0}
+    },
+    "ETHUSDT" :{
+        "decimal_divide": 1e5,
+        "pip_size": 1,
+        "binance_id": "ETHUSDT",
+        "swap_rate":{"long": 0, "short": 0}
+    },
+    "LTCUSDT" :{
+        "decimal_divide": 1e5,
+        "pip_size": 1,
+        "binance_id": "LTCUSDT",
+        "swap_rate":{"long": 0, "short": 0}
+    },
+    "SOLUSDT" :{
+        "decimal_divide": 1e5,
+        "pip_size": 1,
+        "binance_id": "SOLUSDT",
+        "swap_rate":{"long": 0, "short": 0}
+    },
+    "TRXUSDT" :{
+        "decimal_divide": 1e5,
+        "pip_size": 1,
+        "binance_id": "TRXUSDT",
+        "swap_rate":{"long": 0, "short": 0}
+    },
+    "XRPUSDT" :{
+        "decimal_divide": 1e5,
+        "pip_size": 1,
+        "binance_id": "XRPUSDT",
+        "swap_rate":{"long": 0, "short": 0}
+    },
+    'LPTUSDT':{
+        "decimal_divide": 1e5,
+        "pip_size": 1,
+        "binance_id": "LPTUSDT",
+        "swap_rate":{"long": 0, "short": 0}
+    },
+    'FETUSDT':{
+        "decimal_divide": 1e5,
+        "pip_size": 1,
+        "binance_id": "FETUSDT",
+        "swap_rate":{"long": 0, "short": 0}
+    },
+    'FILUSDT':{
+        "decimal_divide": 1e5,
+        "pip_size": 1,
+        "binance_id": "FILUSDT",
+        "swap_rate":{"long": 0, "short": 0}
+    },
+    'HBARUSDT':{
+        "decimal_divide": 1e5,
+        "pip_size": 1,
+        "binance_id": "HBARUSDT",
+        "swap_rate":{"long": 0, "short": 0}
+    },
+    'LINKUSDT':{
+        "decimal_divide": 1e5,
+        "pip_size": 1,
+        "binance_id": "LINKUSDT",
+        "swap_rate":{"long": 0, "short": 0}
+    },
+    'XLMUSDT':{
+        "decimal_divide": 1e5,
+        "pip_size": 1,
+        "binance_id": "XLMUSDT",
+        "swap_rate":{"long": 0, "short": 0}
+    },
+    'NEARUSDT':{
+        "decimal_divide": 1e5,
+        "pip_size": 1,
+        "binance_id": "NEARUSDT",
+        "swap_rate":{"long": 0, "short": 0}
+    },
+    'AAVEUSDT':{
+        "decimal_divide": 1e5,
+        "pip_size": 1,
+        "binance_id": "AAVEUSDT",
+        "swap_rate":{"long": 0, "short": 0}
+    },
+    'UNIUSDT':{
+        "decimal_divide": 1e5,
+        "pip_size": 1,
+        "binance_id": "UNIUSDT",
+        "swap_rate":{"long": 0, "short": 0}
+    },
+    
+}
